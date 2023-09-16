44156ab253b4d148bfa0ecdae5f6d63c195cf512
AdeyBoy123 <groveraden@gmail.com>
2023-09-16 13:06:47 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace countHi
+{
+    internal class Program
+    {
+        static int CountHi(string input)
+        {
+            int count = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input.Substring(i, Math.Min(2, input.Length - i)) == "hi")
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string input = Console.ReadLine();
+
+            int count = CountHi(input);
+
+            Console.WriteLine("The number of times hi occurs is: " + count);
+            Console.ReadLine();
+        }
+    }
+}
